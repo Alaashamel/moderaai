515ba2de25b014c6e32d9a5fa58cdd6d2c18350c
Alaa Shamel <alaashamel32@gmail.com>
2026-07-30 06:57:21 +0000

    feat: add lexical classifier with pluggable transformer backend

---
+++ src/moderaai/__init__.py
@@ -0,0 +1,4 @@
+from .classifier import LexicalClassifier, ModerationResult, TransformerClassifier
+
+__all__ = ["LexicalClassifier", "ModerationResult", "TransformerClassifier"]
+__version__ = "0.1.0"
